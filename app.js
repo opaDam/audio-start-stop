ed69ac46232e464de22e2dfeab682565c16f0e23
opaDam <54212616+opaDam@users.noreply.github.com>
2022-01-12 00:16:42 +0100

    ok

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 $(function () {
   var audio = $("#audio")[0];
-  var list = $(".ul-list")[0];
 
   $(".playBtn").on("click", function () {
     audio.play();
@@ -17,14 +16,11 @@ $(function () {
     this.currentTime = 0;
   });
 
-
-  // audio = document.querySelector('#audio');
-  // const ullist = document.querySelector('.ul-list');
-  list.addEventListener('click', function (e) {
+  $(".ul-list").on("click", function (e) {
     var song = e.target.dataset.song;
     audio.src = "https://opadam.github.io/map/map/" + song;
     audio.toggleAttribute('autoplay');
-  })
-})
+  });
+});
 
 
