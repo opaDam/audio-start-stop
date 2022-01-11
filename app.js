fbe8eed1d41ebf02512313d9fed201d827889231
opadam <54212616+opaDam@users.noreply.github.com>
2022-01-11 14:01:23 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+$(function () {
+  var audio = $("#audio")[0];
+
+  $(".playBtn").on("click", function () {
+    audio.play();
+  });
+
+  $(".pauseBtn").on("click", function () {
+    audio.pause();
+  });
+  $(".stopBtn").on("click", function () {
+    audio.pause();
+    audio.currentTime = 0;
+  });
+  $("#audio").on("ended", function () {
+    this.currentTime = 0;
+  });
+});
