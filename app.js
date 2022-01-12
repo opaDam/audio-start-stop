84932f5f3b06dd66274c92ae247d701f9759a57f
opaDam <54212616+opaDam@users.noreply.github.com>
2022-01-12 13:51:14 +0100

    add

--- app.js
+++ app.js
@@ -1,35 +1,44 @@
-$(function () {
-  var audio = $("#audio")[0];
-  $(".pauseBtn").hide();
+$('document').ready(function () {
+  $(function () {
+    var audio = $("#audio")[0];
+    $(".pauseBtn").hide();
+    $(".stopBtn").hide(1000);
 
-  $(".playBtn").on("click", function () {
+    $(".playBtn").on("click", function () {
       audio.play();
       $(".playBtn").hide(1000);
       $(".pauseBtn").show(1000);
+      $(".stopBtn").show(1000);
       console.log(this);
-  });
+    });
 
-  $(".pauseBtn").on("click", function () {
-    $(".pauseBtn").hide(1000);
-    $(".playBtn").show(1000);
-    audio.pause();
-  });
-  $(".stopBtn").on("click", function () {
-    $(".pauseBtn").hide(1000);
-    $(".playBtn").show(1000);
-    audio.pause();
-    audio.currentTime = 0;
-  });
-  $("#audio").on("ended", function () {
-    this.currentTime = 0;
-  });
+    $(".pauseBtn").on("click", function () {
+      $(".pauseBtn").hide(1000);
+      $(".playBtn").show(1000);
+      $(".stopBtn").hide(1000);
+      audio.pause();
+    });
+    $(".stopBtn").on("click", function () {
+      $(".pauseBtn").hide(1000);
+      $(".playBtn").show(1000);
+      $(".stopBtn").hide(1000);
+      audio.pause();
+      audio.currentTime = 0;
+    });
+    $("#audio").on("ended", function () {
+      this.currentTime = 0;
+    });
 
-  $(".ul-list").on("click", function (e) {
-    var song = e.target.dataset.song;
-    audio.src = "https://opadam.github.io/map/map/" + song;
-    audio.toggleAttribute('autoplay');
-    $(".songPlay")[0].innerText = (e.target.innerText);
+    $(".ul-list").on("click", function (e) {
+      var song = e.target.dataset.song;
+      audio.src = "https://opadam.github.io/map/map/" + song;
+
+      audio.toggleAttribute('autoplay');
+      $(".songPlay")[0].innerText = (e.target.innerText);
+      $(".pauseBtn").show(1000);
+      $(".playBtn").hide(1000);
+      $(".stopBtn").show(1000);
+    });
   });
 });
-
 
