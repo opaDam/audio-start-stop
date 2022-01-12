29d85160e67213df47eb7112eff9bbc4cb55d216
opaDam <54212616+opaDam@users.noreply.github.com>
2022-01-12 13:09:59 +0100

    add

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 $(function () {
   var audio = $("#audio")[0];
+  $(".pauseBtn").hide();
 
   $(".playBtn").on("click", function () {
       audio.play();
+      $(".playBtn").hide(1000);
+      $(".pauseBtn").show(1000);
+      console.log(this);
   });
 
   $(".pauseBtn").on("click", function () {
+    $(".pauseBtn").hide(1000);
+    $(".playBtn").show(1000);
     audio.pause();
   });
   $(".stopBtn").on("click", function () {
+    $(".pauseBtn").hide(1000);
+    $(".playBtn").show(1000);
     audio.pause();
     audio.currentTime = 0;
   });
